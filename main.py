9f6669cfc2b7d6a59b7789c95aa387f21def9d1e
u202210494 <u202210494@upc.edu.pe>
2024-03-24 15:21:24 -0500

    Commit inicial

---
+++ main.py
@@ -0,0 +1,13 @@
+from Plano import Plano
+
+# ecuacion = "2 x + 4y+12z + 3 = 0"
+ecuacion = input()
+plano = Plano(ecuacion)
+
+print(f"Input: \"{ecuacion}\"\n")
+print(f"Ecuacion: \"{plano.ecuacion}\"\n")
+print(f"a: {plano.ecuacion.a}")
+print(f"b: {plano.ecuacion.b}")
+print(f"c: {plano.ecuacion.c}")
+print(f"d: {plano.ecuacion.d}")
+print(f"=: {plano.ecuacion.igual}")
