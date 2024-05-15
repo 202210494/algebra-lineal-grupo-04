3b0206305d2b78d568f9ddd70829d27a541a1029
u202210494 <u202210494@upc.edu.pe>
2024-05-15 12:54:42 -0500

    Correción menor

--- clases/Plano.py
+++ clases/Plano.py
@@ -2,8 +2,8 @@ import clases.Ecuacion as e
 import clases.Vector as v
 
 class Plano:
-    def __init__(self, input):
-        self.ecuacion = e.Ecuacion(input)
+    def __init__(self, input_str):
+        self.ecuacion = e.Ecuacion(input_str)
         self.normal = v.Vector3D(x=self.ecuacion.a, y=self.ecuacion.b, z=self.ecuacion.c)
 
     def __str__(self):
