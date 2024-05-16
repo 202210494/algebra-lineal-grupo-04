9cc771e2ea6f2b9c1e1737f23df3915249008c79
R0SEWT <rody.vilchez00@gmail.com>
2024-05-15 22:22:45 -0500

    proto gui con rotaciones

--- src/view.py
+++ src/view.py
@@ -5,7 +5,7 @@ import numpy as np
 class ProyeccionView:
     def __init__(self, root):
         self.root = root
-        self.root.title("Proyección Ortogonal de un Punto sobre un Plano")
+        self.root.title("Calculadora de Proyección Ortogonal y Rotaciones")
 
         self.frame_punto = tk.Frame(root)
         self.frame_punto.pack(pady=10)
@@ -23,28 +23,65 @@ class ProyeccionView:
         self.entry_pz = tk.Entry(self.frame_punto)
         self.entry_pz.grid(row=1, column=5)
 
+        ############################## Ecucacion del plano ##############################
+        # Todo: disminuir el tamaño de los entrys (padx=5, pady=3) (ir probando con valores)
+        # sino mover el columnspan=3
+
         self.frame_plano = tk.Frame(root)
         self.frame_plano.pack(pady=10)
-        tk.Label(self.frame_plano, text="Plano (ax + by + cz + d = 0):").grid(row=0, columnspan=8)
+        tk.Label(self.frame_plano, text="Plano (ax + by + cz + d = 0):").grid(row=0, columnspan=5)
 
-        tk.Label(self.frame_plano, text="a:").grid(row=1, column=0)
         self.entry_a = tk.Entry(self.frame_plano)
-        self.entry_a.grid(row=1, column=1)
+        self.entry_a.grid(row=1, column=0, padx=5) 
+        tk.Label(self.frame_plano, text="x + ").grid(row=1, column=1)
+        
 
-        tk.Label(self.frame_plano, text="b:").grid(row=1, column=2)
+        
         self.entry_b = tk.Entry(self.frame_plano)
-        self.entry_b.grid(row=1, column=3)
+        self.entry_b.grid(row=1, column=2, padx=2)
+        tk.Label(self.frame_plano, text="y + ").grid(row=1, column=3)
 
-        tk.Label(self.frame_plano, text="c:").grid(row=1, column=4)
         self.entry_c = tk.Entry(self.frame_plano)
-        self.entry_c.grid(row=1, column=5)
-
-        tk.Label(self.frame_plano, text="d:").grid(row=1, column=6)
+        self.entry_c.grid(row=1, column=4)
+        tk.Label(self.frame_plano, text="z + ").grid(row=1, column=5)
+        
         self.entry_d = tk.Entry(self.frame_plano)
-        self.entry_d.grid(row=1, column=7)
+        self.entry_d.grid(row=1, column=6, padx=7)      
 
+        tk.Label(self.frame_plano, text="= 0").grid(row=1, column=7)
+
+        # TODO: leer ecucacion del plano en un solo entry
+
+        self.frame_plano2 = tk.Frame(root)
+        self.frame_plano2.pack(pady=10)
+        tk.Label(self.frame_plano2, text="Ecuacion del Plano:").grid(row=0, columnspan=8)
+        tk.Label(self.frame_plano2, text="(ax + by + cz + d = 0):").grid(row=1, column=0)
+        self.entry_plano = tk.Entry(self.frame_plano2)
+        self.entry_plano.grid(row=1, column=1)
+    
         self.calc_button = tk.Button(root, text="Calcular y Graficar")
         self.calc_button.pack(pady=10)
+
+        self.frame_rotaciones = tk.Frame(root)
+        self.frame_rotaciones.pack(pady=10)
+        tk.Label(self.frame_rotaciones, text="Rotaciones sexadecimal o radian?:").grid(row=0, columnspan=8) # columnspan=8 para que ocupe todo el ancho
+        
+        tk.Label(self.frame_rotaciones, text="Rotacion en X:").grid(row=1, column=0)
+        self.entry_rot_x = tk.Entry(self.frame_rotaciones)
+        self.entry_rot_x.grid(row=1, column=1)
+
+        tk.Label(self.frame_rotaciones, text="Rotacion en Y:").grid(row=1, column=2)
+        self.entry_rot_y = tk.Entry(self.frame_rotaciones)
+        self.entry_rot_y.grid(row=1, column=3)
+
+        tk.Label(self.frame_rotaciones, text="Rotacion en Z:").grid(row=1, column=4)
+        self.entry_rot_z = tk.Entry(self.frame_rotaciones)
+        self.entry_rot_z.grid(row=1, column=5)
+        
+        self.calc_button_rot = tk.Button(root, text="Calcular Proyeccion con Rotaciones")
+        self.calc_button_rot.pack(pady=10)
+
+
 
     def get_punto(self):
         try:
