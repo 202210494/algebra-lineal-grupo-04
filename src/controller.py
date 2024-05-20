add dinamic size & fix rotation

src/controller.py
@@ -9,15 +9,19 @@ class ProyeccionController:
     def __init__(self, root):
         self.model = ProyeccionModel()
         self.view = ProyeccionView(root)
+
         self.view.calc_button.config(command=self.calcular_proyeccion)
         self.view.calc_button_rot.config(command=self.calcular_proyeccion_rotacion)
 
     def calcular_proyeccion(self):
         P_P = self.view.get_punto()
         PL_M = self.view.get_plano()
+
         if P_P is not None and PL_M is not None:
             plano, vectores, puntos = self.model.calcular_proyeccion(P_P, PL_M)
-            self.animar(plano, vectores, puntos)
+            size = self.model.calc_max_v()
+            self.model.describir_proyeccion(PL_M)
+            self.animar(plano, vectores, puntos, size)
 
     def calcular_proyeccion_rotacion(self):
         P_P = self.view.get_punto()
@@ -26,10 +30,12 @@ class ProyeccionController:
         
         if P_P is not None and PL_M is not None and rotacion is not None:
             plano, vectores, puntos = self.model.calc_con_rotacion(P_P, PL_M, rotacion)
-            self.animar(plano, vectores, puntos, rotacion)
+            size = self.model.calc_max_v()
+            self.model.describir_proyeccion(PL_M)
+            self.animar(plano, vectores, puntos, size, rotacion)
 
 
-    def animar(self, plano, vectores, puntos, rotacion=None):
+    def animar(self, plano, vectores, puntos, size ,rotacion=None):
         #mpl.rcParams.update(mpl.rcParamsDefault)
         #plt.style.use("default")
         plt.style.use("tableau-colorblind10")
@@ -37,20 +43,24 @@ class ProyeccionController:
         #plt.style.use("bmh") # https://python-charts.com/es/matplotlib/estilos/#google_vignette
         
         fig = plt.figure()
-        ax = fig.add_subplot(111, projection="3d", autoscale_on=True)
+        if rotacion is not None:
+            ax = fig.add_subplot(122, projection="3d")
+            ax1 = fig.add_subplot(121, projection="3d")
+        else:
+            ax = fig.add_subplot(111, projection="3d", autoscale_on=True)
 
-        # pintamos plano con meshgrid
-
-        X, Y = np.meshgrid(np.linspace(-5, 5), np.linspace(-5, 5))
+        X, Y = np.meshgrid(np.linspace(-size, size), np.linspace(-size, size))
         Z = -(plano[0] * X + plano[1] * Y + plano[3]) / plano[2] # Ecuacion del plano (tomamos z como la variable dependiente)
 
         ax.plot_surface(X, Y, Z, color="yellow", alpha=0.5)
 
-        def update_s_r(frame):
+        if rotacion is not None:
+            ax1.plot_surface(X, Y, Z, color="yellow", alpha=.5)
+
+        def _update_proyeccion(frame):
                           
             ax.clear()
             ax.plot_surface(X, Y, Z, color="yellow", alpha=0.5)
-
 
             if frame >= .5:
                 punto = puntos["P"]
@@ -58,7 +68,6 @@ class ProyeccionController:
                 ax.text(punto[0], punto[1], punto[2], f'P ({round(punto[0],2)}, {round(punto[1],2)}, {round(punto[2],2)})', # 
                         fontsize=8, ha='center', va='bottom', color='black')
                 
-
             if frame >= 1 + sep:
                 punto = puntos["P_0"]
                 ax.scatter(punto[0], punto[1], punto[2], c='blue', s=10)
@@ -90,60 +99,69 @@ class ProyeccionController:
                         fontsize=8, ha='center', va='bottom', color='black')    
             #ax.legend(loc='lower left') no se puede colocar indice sin warning
 
-        def update_con_rotacion(frame):
-                ax.clear()
-                ax.plot_surface(X, Y, Z, color="yellow", alpha=0.5)
+        def _update_proyeccion_rotacion(frame):
+                          
+            ax.clear()
+            ax.plot_surface(X, Y, Z, color="yellow", alpha=0.5)
+            ax1.clear()
+            ax1.plot_surface(X, Y, Z, color="yellow", alpha=0.5)
 
-                if frame >= .5:
-                    punto = puntos["P"]
-                    ax.scatter(punto[0], punto[1], punto[2], c='yellow', s=10)
-                    ax.text(punto[0], punto[1], punto[2], f'P ({round(punto[0],2)}, {round(punto[1],2)}, {round(punto[2],2)})', # 
-                            fontsize=8, ha='center', va='bottom', color='black')
+            if frame >= 2:
+                punto = puntos["P"]
+                ax.scatter(punto[0], punto[1], punto[2], c='yellow', s=10)
+                ax.text(punto[0], punto[1], punto[2], f'P ({round(punto[0],2)}, {round(punto[1],2)}, {round(punto[2],2)})', # 
+                        fontsize=8, ha='center', va='bottom', color='black')
+                
+            if frame >= 2 + sep:
+                punto = puntos["P_0"]
+                ax.scatter(punto[0], punto[1], punto[2], c='blue', s=10)
+                ax.text(punto[0], punto[1], punto[2], f'P ({round(punto[0],2)}, {round(punto[1],2)}, {round(punto[2],2)})', #                         
+                        fontsize=8, ha='center', va='bottom', color='black')
 
-                if frame >= 1 + sep:
-                    punto = puntos["P_0"]
-                    ax.scatter(punto[0], punto[1], punto[2], c='blue', s=10)
-                    ax.text(punto[0], punto[1], punto[2], f'P ({round(punto[0],2)}, {round(punto[1],2)}, {round(punto[2],2)})', #                         
-                            fontsize=8, ha='center', va='bottom', color='black')
+            if frame >= 2 + 2 * sep:
+                vector = vectores["u"]
+                ax.quiver(vector[0], vector[1], vector[2], vector[3], vector[4], 
+                          vector[5], color="purple", arrow_length_ratio=0.1,
+                          label="Vector u (P-P0)")
 
-                if frame >= 1 + 2 * sep:
-                    vector = vectores["u"]
-                    ax.quiver(vector[0], vector[1], vector[2], vector[3], vector[4], 
-                              vector[5], color="purple", arrow_length_ratio=0.1,
-                              label="Vector u (P-P0)")
+            if frame >= 2 + 3 * sep:
+                vector = vectores["n"]
+                ax.quiver(vector[0], vector[1], vector[2], vector[3], vector[4], vector[5], color="orange", arrow_length_ratio=0.1,
+                          label="vector n (normal al plano)")
+                vector = vectores["-n"]
+                ax.quiver(vector[0], vector[1], vector[2], vector[3], vector[4], vector[5], color="orange", arrow_length_ratio=0.1)
 
-                if frame >= 1 + 3 * sep:
-                    vector = vectores["n"]
-                    ax.quiver(vector[0], vector[1], vector[2], vector[3], vector[4], vector[5], color="orange", arrow_length_ratio=0.1,
-                              label="vector n (normal al plano)")
-                    vector = vectores["-n"]
-                    ax.quiver(vector[0], vector[1], vector[2], vector[3], vector[4], vector[5], color="orange", arrow_length_ratio=0.1)
+            if frame >= 2 + 4 * sep:
+                vector = vectores["proy_n_u"]
+                ax.quiver(vector[0], vector[1], vector[2], vector[3], vector[4], vector[5], color="green", arrow_length_ratio=0.1,
+                          label="Proyeccion ortogonal")
 
-                if frame >= 1 + 4 * sep:
-                    vector = vectores["proy_n_u"]
-                    ax.quiver(vector[0], vector[1], vector[2], vector[3], vector[4], vector[5], color="green", arrow_length_ratio=0.1,
-                              label="Proyeccion ortogonal")
-
-                if frame >= 1 + 5 * sep:
-                    punto = puntos["P_"]
-                    ax.scatter(punto[0], punto[1], punto[2], c='red', s=10)        
-                    ax.text(punto[0], punto[1], punto[2], f'P ({round(punto[0],2)}, {round(punto[1],2)}, {round(punto[2],2)})', #                         
-                            fontsize=8, ha='center', va='bottom', color='black')   
-
-                if frame >= 1 + 6 * sep:
-                    punto = puntos["P_r"]
-                    ax.scatter(punto[0], punto[1], punto[2], c='green', s=10)        
-                    ax.text(punto[0], punto[1], punto[2], f'P ({round(punto[0],2)}, {round(punto[1],2)}, {round(punto[2],2)})', #                         
-                            fontsize=8, ha='center', va='bottom', color='black') 
+            if frame >= 2 + 5 * sep:
+                punto = puntos["P_"]
+                ax.scatter(punto[0], punto[1], punto[2], c='red', s=10)        
+                ax.text(punto[0], punto[1], punto[2], f'P ({round(punto[0],2)}, {round(punto[1],2)}, {round(punto[2],2)})', #                         
+                        fontsize=8, ha='center', va='bottom', color='black')    
+            
+            if frame >= .5:
+                punto = puntos["P_Rotar"]
+                ax1.scatter(punto[0], punto[1], punto[2], c='red', s=20)
+                ax1.text(punto[0], punto[1], punto[2], f'P ({round(punto[0],2)}, {round(punto[1],2)}, {round(punto[2],2)})', # 
+                        fontsize=8, ha='center', va='bottom', color='black')
+            
+            if frame >= 1.5:
+                punto = puntos["P"]
+                ax1.scatter(punto[0], punto[1], punto[2], c='yellow', s=20)
+                ax1.text(punto[0], punto[1], punto[2], f'P ({round(punto[0],2)}, {round(punto[1],2)}, {round(punto[2],2)})', # 
+                        fontsize=8, ha='center', va='bottom', color='black')
 
         sep = 2
         ext_int = 2
 
         if rotacion is not None:
-            ani2 = FuncAnimation(fig, update_con_rotacion, frames=range((8+ext_int) * sep), interval=1000)
-            ani2.save("proyeccion_rotacion.gif", fps=10)
+            ani1 = FuncAnimation(fig, _update_proyeccion_rotacion, frames=range((8+ext_int) * sep), interval=1000)
+            ani1.save("proyeccion_rotacion.gif", fps=10)
         else:
-            ani = FuncAnimation(fig, update_s_r, frames=range((7+ext_int) * sep), interval=1000)
+            ani = FuncAnimation(fig, _update_proyeccion, frames=range((7+ext_int) * sep), interval=1000)
             ani.save("proyeccion.gif", fps=10)
         
         plt.show()

src/model.py
@@ -94,13 +94,10 @@ class ProyeccionModel:
         # hallar punto proyectado P'
         P_P_ = P_P + proy_n_u
         self.puntos["P_"] = P_P_
-
-        self.describir_proyeccion(PL_M)
         
         # mandamos el plano, los vectores y los puntos
         return PL_M, self.vectores, self.puntos 
 
- 
 
     def rotar_punto(self, punto, rotacion):
 
@@ -121,17 +118,34 @@ class ProyeccionModel:
         return np.dot(R, punto) # Devolvemos el punto rotado en los 3 ejes
 
     def calc_con_rotacion(self, P_P, PL_M, rotacion):
-        # calculamos la proyeccion
-        PL_M, vectores, puntos = self.calcular_proyeccion(P_P, PL_M)
-        # rotamos el punto proyectado
-        puntos["P_r"] = self.rotar_punto(puntos["P_"], rotacion)
-        print("Rotacion del punto proyectado:")
+        # Rotamos el punto inicial y lo guardamos como punto a proyectar
+        self.puntos["P"] = self.rotar_punto(P_P, rotacion)
+
+        self.puntos["P_Rotar"] = P_P
+
+        # calculamos la proyeccion desde el nuevo punto 
+        self.calcular_proyeccion(self.puntos["P"], PL_M)
+        
+        print("Rotacion en rad del punto a proyectar:")
         print("------------------------------")
         print(f"Rotacion en X: {rotacion[0]}")
         print(f"Rotacion en Y: {rotacion[1]}")
         print(f"Rotacion en Z: {rotacion[2]}\n")
-        print(f"Punto proyectado rotado: \n{self.parsed_point(puntos['P_r'])}\n")
+        print(f"Punto ha rotar: {self.parsed_point(P_P)}")
+        print(f"Punto proyectado rotado: \n{self.parsed_point(self.puntos['P'])}\n")
+        
+        self.describir_proyeccion(PL_M)
+
         #print("En la animacion puedes ver como se proyecta el punto P sobre el plano y luego se rota\n")
 
-        return PL_M, vectores, puntos
+        return PL_M, self.vectores, self.puntos
+
+    def calc_max_v(self):
+        m_s = 0
+        for v in self.vectores.values():
+            c_s = np.sqrt(v[3]**2 + v[4]**2 + v[5]**2) # get modulo
+            if(c_s > m_s):
+                m_s = c_s
+
+        return m_s
 

src/view.py
@@ -20,7 +20,7 @@ class ProyeccionView:
         self.style.map("TButton", background=[('active', "#1A4B27"), ('pressed', "#1A2A4B")])  # Cambiar color al pasar el mouse
                              
         
-        self.style.configure("TEntry", fieldbackground="#4D4D4D", foreground="#FFFFFF", insertbackground="cyan", selectbackground="blue", selectforeground="white")
+        self.style.configure("TEntry", fieldbackground="#4D4D4D", foreground="#FFFFFF", insertbackground="cyan", selectbackground="blue", selectforeground="white") 
 
         self.root.configure(bg="#2E2E2E")
 
@@ -119,7 +119,8 @@ class ProyeccionView:
 
     def get_punto(self):
         try:
-            P_P = np.array([float(self.entry_px.get()), float(self.entry_py.get()), float(self.entry_pz.get())])
+            P_P = np.array([1,0,0]) #dev
+            #P_P = np.array([float(self.entry_px.get()), float(self.entry_py.get()), float(self.entry_pz.get())])
             return P_P
         except ValueError:
             messagebox.showerror("Input Error", "Por favor, ingrese números válidos para el punto.")
@@ -127,7 +128,8 @@ class ProyeccionView:
 
     def get_plano(self):
         try:
-            PL_M = np.array([float(self.entry_a.get()), float(self.entry_b.get()), float(self.entry_c.get()), float(self.entry_d.get())])
+            PL_M = np.array([1,2,3,4])
+            #PL_M = np.array([float(self.entry_a.get()), float(self.entry_b.get()), float(self.entry_c.get()), float(self.entry_d.get())])
             return PL_M
         except ValueError:
             messagebox.showerror("Input Error", "Por favor, ingrese números válidos para el plano.")
@@ -139,6 +141,7 @@ class ProyeccionView:
             rot_x = float(self.entry_rot_x.get()) if self.entry_rot_x.get() else 0
             rot_y = float(self.entry_rot_y.get()) if self.entry_rot_y.get() else 0
             rot_z = float(self.entry_rot_z.get()) if self.entry_rot_z.get() else 0
+            rot_z = 180
             return np.radians([rot_x, rot_y, rot_z]) # convertir a radianes
         except ValueError:
             messagebox.showerror("Input Error", "Por favor, ingrese números válidos para las rotaciones.")